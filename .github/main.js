5a3ca0ce8082c4e22a1634481361bff7c5e364b1
elena souto <elenasouto@gmail.com>
2021-11-19 13:38:35 +0000

    Added user input and computer input

---
+++ .github/main.js
@@ -0,0 +1,72 @@
+
+
+//if (playerMove === "scissors"){
+//    console.log ("you lost")
+//}
+
+//if (playerMove === "paper"){
+//    console.log ("you won")
+//}
+
+//if (playerMove === "rock"){
+//    console.log ("you draw")
+//}
+//1. Get the players move
+
+//2. Get the computers move
+
+//3. What is the players move rock, paper, scissors
+//4.What is the computers move rock, paper, scissors
+//5. Compare together the results 
+
+
+
+const moves = ["rock", "paper", "scissors"];
+
+
+let playerMove = prompt ("Please enter your move")
+let computerMove = moves[ Math.floor((Math.random() * 3))]
+console.log (computerMove)
+
+
+
+if (playerMove===computerMove) {
+console.log ("it's a draw")
+
+}
+// if playermove is rock and computermove is paper winner is computer
+
+if ("playerMove===rock")
+
+
+
+
+
+
+// //if (playerMove === "scissors"){
+// console.log ("you lost");
+// } 
+// else if (playerMove === "paper") {
+//     console.log("you won");}
+// else if (playerMove === "rock"){
+//     console.log ("you draw"); 
+// } 
+
+
+// if (playerMove === "scissors"){
+//     console.log ("you draw");
+//     } 
+//     else if (playerMove === "paper") {
+//         console.log("you lost");}
+//     else if (playerMove === "rock"){
+//         console.log ("you won");
+//     }
+
+
+
+
+
+
+
+
+
