c2fec8eddb62bfadde7b79aa5ce525aa5f866287
ivanf <ivan.fanello@gmail.com>
2021-12-21 10:03:19 +0000

    all done

--- .github/main.js
+++ .github/main.js
@@ -1,137 +1,96 @@
-
-
-//if (playerMove === "scissors"){
-//    console.log ("you lost")
-//}
-
-//if (playerMove === "paper"){
-//    console.log ("you won")
-//}
-
-//if (playerMove === "rock"){
-//    console.log ("you draw")
-//}
-//1. Get the players move
-
-//2. Get the computers move
-
-//3. What is the players move rock, paper, scissors
-//4.What is the computers move rock, paper, scissors
-//5. Compare together the results 
-
-
-
+let playerMove;
+let computerMove;
 const moves = ["rock", "paper", "scissors"];
+function conditionsPlayerWins() {
+  return (
+    (playerMove === "rock" && computerMove === "scissors") ||
+    (playerMove === "paper" && computerMove === "rock") ||
+    (playerMove === "scissors" && computerMove === "paper")
+  );
+}
+function conditionsComputerWins() {
+  return (
+    (playerMove === "rock" && computerMove === "paper") ||
+    (playerMove === "paper" && computerMove === "scissors") ||
+    (playerMove === "scissors" && computerMove === "rock")
+  );
+}
+const conditionsToDraw = playerMove === computerMove;
 let playAgain = true;
+let numberOfGames = 0;
+let playerWins = 0;
+let computerWins = 0;
+let gameDraw = 0;
 
-// add a while loop to make the game continuous
-
-while (playAgain) {
-let playerMove = prompt ("Please enter your move")
-let computerMove = moves[ Math.floor((Math.random() * 3))]
-console.log (computerMove)
-
-
-
-
-
-if (playerMove === computerMove) {
-    alert("0")
-
-}
-// if playermove is rock and computermove is paper winner is computer
-
-if (playerMove === "rock" && computerMove === "paper") {
-    alert("-1")
-};
-//// if playermove is rock and computermove is scissors winner is player
-
-
-if (playerMove === "rock" && computerMove === "scissors") {
-    alert("1")
+function getWinner(player1, player2) {
+  playerMove = player1;
+  computerMove = player2;
+  console.log(playerMove, computerMove);
+  if (conditionsPlayerWins()) {
+    return 1;
+  } else if (conditionsComputerWins()) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
-// if playermove is paper and computermove is scissors winner is computer
-if (playerMove === "paper" && computerMove === "scissors") {
-    alert("-1")
+//console.log(getWinner("paper", "paper"));
+
+function computerHasWon() {
+  computerWins++;
+  alert(-1);
+  alert(
+    "computerwins " +
+      computerWins +
+      " Player Wins " +
+      playerWins +
+      " Game Draw " +
+      gameDraw
+  );
+}
+function playerHasWon() {
+  playerWins++;
+  alert(1);
+  alert(
+    "Player Wins " +
+      playerWins +
+      " computerwins " +
+      computerWins +
+      " Game Draw " +
+      gameDraw
+  );
+}
+function weDraw() {
+  gameDraw++;
+  alert(0);
+  alert(
+    "It's a draw " +
+      gameDraw +
+      " computerwins " +
+      computerWins +
+      " Player Wins " +
+      playerWins
+  );
 }
 
+function getWinnerByUI() {
+  while (playAgain) {
+    playerMove = prompt("Please enter your move");
+    computerMove = moves[Math.floor(Math.random() * 3)];
+    numberOfGames++;
 
-// if playermove is paper and computermove is rock winner is player
-if (playerMove === "paper" && computerMove === "rock") {
-    alert("1")
-}
+    if (conditionsPlayerWins()) {
+      playerHasWon();
+    } else if (conditionsComputerWins()) {
+      computerHasWon();
+    } else {
+      weDraw();
+    }
 
-// if playermove is scissors and computermove is paper winner is player
-if (playerMove === "scissors" && computerMove === "paper") {
-    alert("1")
-}
-
-
-// if playermove is scissors and computermove is rock winner is computer
-if (playerMove === "scissors" && computerMove === "rock") {
-    alert("-1")
-}
-    // prompt("Would you like to play again yes or no?") {
-    // if (playAgain === yes === true)}; {
-    // else (playAgain === no ===  false)}
-
-    // prompt("Would you like to play again yes or no?")
-    // if (playAgain === "yes"){
-    //     playAgain = true;
-    // } else if (playAgain === "no")
-    // {
-    //     playAgain = false;
-    // }
-
+    alert("you played " + numberOfGames + " games");
+    console.log(computerMove);
     playAgain = confirm("would you like to play again??");
+  }
 }
-
-    // window.confirm( "Would you like to play again?" );
-    // if (confirm === true){
-    //     playAgain === true;
-    // } else {
-    //     playAgain === false;
-    // } break;
-    
-
-
-
-
-
-
-
-
-// ask the player if he wants to play again
-//
-
-
-
-
-// //if (playerMove === "scissors"){
-// console.log ("you lost");
-// } 
-// else if (playerMove === "paper") {
-//     console.log("you won");}
-// else if (playerMove === "rock"){
-//     console.log ("you draw"); 
-// } 
-
-
-// if (playerMove === "scissors"){
-//     console.log ("you draw");
-//     } 
-//     else if (playerMove === "paper") {
-//         console.log("you lost");}
-//     else if (playerMove === "rock"){
-//         console.log ("you won");
-//     }
-
-
-
-
-
-
-
-
-
+getWinnerByUI();
